35c3da8975138d93dfe394159a4f59426006335e
Lcnvdl <programando.soft@gmail.com>
2020-04-02 03:36:01 -0300

    Create SetActive.cs

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Actions/GameObjects/SetActive.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SetActive : ActionScript
+{
+    public GameObject target;
+
+    public bool enable = true;
+
+    protected override void Run()
+    {
+        (target ?? gameObject).SetActive(enable);
+    }
+}
