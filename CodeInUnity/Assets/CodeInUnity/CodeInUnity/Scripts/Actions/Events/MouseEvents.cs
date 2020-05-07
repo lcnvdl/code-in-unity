e11f5d9be887ce6c79c171e87c2b3a43dd034cef
Lcnvdl <programando.soft@gmail.com>
2020-05-07 20:55:28 -0300

    Mouse events

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Actions/Events/MouseEvents.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+    public UnityEvent mouseDown;
+    public UnityEvent mouseEnter;
+    public UnityEvent mouseLeave;
+    public UnityEvent mouseUp;
+
+    void OnMouseUp()
+    {
+        mouseUp?.Invoke();
+    }
+
+    void OnMouseDown()
+    {
+        mouseDown?.Invoke();
+    }
+
+    void OnMouseOver()
+    {
+        mouseEnter?.Invoke();
+    }
+
+    void OnMouseExit()
+    {
+        mouseLeave?.Invoke();
+    }
+}
