b1e6baa0765366626d022b31fac8f9fa49e80f98
Lcnvdl <programando.soft@gmail.com>
2022-12-09 11:46:23 -0300

    Bugfix

--- CodeInUnity/Assets/CodeInUnity/CodeInUnity/Core/Extensions/UnityExtensions.cs
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Core/Extensions/UnityExtensions.cs
@@ -1,15 +1,16 @@
 using CodeInUnity.Utils;
+using UnityEngine;
 
 namespace CodeInUnity.Extensions
 {
-  public static class UnityExtensions
-  {
-    /// <summary>
-    /// Extension method to check if a layer is in a layermask
-    /// </summary>
-    public static bool Contains(this LayerMask mask, int layer)
+    public static class UnityExtensions
     {
-      return LayerMaskUtils.Contains(mask, layer);
+        /// <summary>
+        /// Extension method to check if a layer is in a layermask
+        /// </summary>
+        public static bool Contains(this LayerMask mask, int layer)
+        {
+            return LayerMaskUtils.Contains(mask, layer);
+        }
     }
-  }
 }
