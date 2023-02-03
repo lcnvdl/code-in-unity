ada99f56921f6a1a2375b031e81d9f3392e3fb10
Lcnvdl <programando.soft@gmail.com>
2023-02-02 21:54:20 -0300

    Create SetAnimatorIntValue.cs

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Actions/Animators/SetAnimatorIntValue.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class SetAnimatorIntValue : ActionScript
+{
+    public Animator target;
+
+    public string variableName;
+
+    public int targetValue;
+
+    public void RunWithNameAndValue(string overrideName, int overrideValue)
+    {
+        this.variableName = overrideName;
+        this.targetValue = overrideValue;
+
+        this.ExecuteAction();
+    }
+
+    public void RunWithValue(int overrideValue)
+    {
+        this.targetValue = overrideValue;
+        this.ExecuteAction();
+    }
+
+    protected override void Run()
+    {
+        if (target == null)
+        {
+            target = GetComponent<Animator>();
+        }
+
+        if (target != null)
+        {
+            target.SetInteger(variableName, targetValue);
+        }
+    }
+}
