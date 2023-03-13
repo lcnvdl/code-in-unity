c8a7f793533c7c8de3e9255c276b71ff73fac675
Lcnvdl <programando.soft@gmail.com>
2023-03-13 11:37:18 -0300

    Create OcclusionToggler.cs

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Mesh/OcclusionToggler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum OcclusionStatus
+{
+    Enabled,
+    Disabled
+}
+
+public class OcclusionToggler : MonoBehaviour
+{
+    public OcclusionStatus status;
+
+    public bool selfApply = true;
+
+    public bool recursive = true;
+
+    void Start()
+    {
+        if (this.selfApply)
+        {
+            if (TryGetComponent<MeshRenderer>(out var mesh))
+            {
+                mesh.allowOcclusionWhenDynamic = (status == OcclusionStatus.Enabled);
+            }
+        }
+
+        if (this.recursive)
+        {
+            foreach (var mesh in GetComponentsInChildren<MeshRenderer>())
+            {
+                mesh.allowOcclusionWhenDynamic = (status == OcclusionStatus.Enabled);
+            }
+        }
+    }
+}
