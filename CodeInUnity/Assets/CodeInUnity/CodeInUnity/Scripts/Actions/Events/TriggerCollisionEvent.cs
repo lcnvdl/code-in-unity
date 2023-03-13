cf6fc3bb03955a2d3aacb919c6203b20ed930ea7
Lcnvdl <programando.soft@gmail.com>
2023-03-13 11:37:56 -0300

    Update TriggerCollisionEvent.cs

--- CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Actions/Events/TriggerCollisionEvent.cs
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Actions/Events/TriggerCollisionEvent.cs
@@ -6,12 +6,26 @@ using UnityEngine.Events;
 
 public class TriggerCollisionEvent : MonoBehaviour
 {
+    public string objectNameTest;
+
+    public string tagTest;
+
     public ColliderEvent onTriggerEnter;
 
     public ColliderEvent onTriggerExit;
 
     private void OnTriggerExit(Collider other)
     {
+        if (!string.IsNullOrEmpty(objectNameTest) && other.gameObject.name != objectNameTest)
+        {
+            return;
+        }
+
+        if (!string.IsNullOrEmpty(tagTest) && !other.CompareTag(tagTest))
+        {
+            return;
+        }
+        
         if (this.onTriggerExit != null)
         {
             this.onTriggerExit.Invoke(other);
@@ -20,6 +34,16 @@ public class TriggerCollisionEvent : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!string.IsNullOrEmpty(objectNameTest) && other.gameObject.name != objectNameTest)
+        {
+            return;
+        }
+
+        if (!string.IsNullOrEmpty(tagTest) && !other.CompareTag(tagTest))
+        {
+            return;
+        }
+
         if (this.onTriggerEnter != null)
         {
             this.onTriggerEnter.Invoke(other);
