174b74c05289ed195345ee7dc5d935e60f3d5e87
Lcnvdl <programando.soft@gmail.com>
2023-03-29 12:00:57 -0300

    Collision event

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Actions/Events/OnCollisionEvent.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnCollisionEvent : MonoBehaviour
+{
+    public string objectNameTest;
+
+    public string tagTest;
+
+    public CollisionEvent onCollisionEnter;
+
+    public CollisionEvent onCollisionExit;
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (!string.IsNullOrEmpty(objectNameTest) && other.gameObject.name != objectNameTest)
+        {
+            return;
+        }
+
+        if (!string.IsNullOrEmpty(tagTest) && !other.gameObject.CompareTag(tagTest))
+        {
+            return;
+        }
+        
+        if (this.onCollisionExit != null)
+        {
+            this.onCollisionExit.Invoke(other);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!string.IsNullOrEmpty(objectNameTest) && other.gameObject.name != objectNameTest)
+        {
+            return;
+        }
+
+        if (!string.IsNullOrEmpty(tagTest) && !other.gameObject.CompareTag(tagTest))
+        {
+            return;
+        }
+
+        if (this.onCollisionEnter != null)
+        {
+            this.onCollisionEnter.Invoke(other);
+        }
+    }
+}
