16832fdc758c87945a0572b6b378ce39b62a5aa5
Lcnvdl <programando.soft@gmail.com>
2023-03-29 12:01:35 -0300

    Create SeededRandom.cs

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Core/Security/SeededRandom.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using S = System;
+
+namespace CodeInUnity.Core.Security
+{
+    [S.Serializable]
+    public class SeededRandom
+    {
+        [SerializeField]
+        [HideInInspector]
+        private int currentSeed = 1;
+
+        public int Seed
+        {
+            get => this.currentSeed;
+            set
+            {
+                this.randomInstance = null;
+                this.currentSeed = value;
+            }
+        }
+
+        private S.Random randomInstance = null;
+
+        private S.Random RandomInstance => this.randomInstance ?? (this.randomInstance = new S.Random(this.currentSeed));
+
+        public float Range(float minInclusive, float maxInclusive)
+        {
+            double next = this.RandomInstance.NextDouble();
+            return minInclusive + (float)((maxInclusive - minInclusive) * next);
+        }
+
+        public float Next(float maxInclusive)
+        {
+            double next = this.RandomInstance.NextDouble();
+            return (float)(maxInclusive * next);
+        }
+
+        public int Range(int minInclusive, int maxExclusive)
+        {
+            return this.RandomInstance.Next(minInclusive, maxExclusive);
+        }
+
+        public int Next(int maxExclusive)
+        {
+            return this.RandomInstance.Next(maxExclusive);
+        }
+    }
+}
