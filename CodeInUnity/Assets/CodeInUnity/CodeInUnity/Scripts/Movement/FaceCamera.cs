1bb0cd5b908c7115878eae6805ae9a992cc429e1
Lcnvdl <programando.soft@gmail.com>
2023-03-29 12:01:12 -0300

    FaceCamera and FollowTarget

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Movement/FaceCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    public Camera Camera;
+
+    private void Update()
+    {
+        transform.LookAt(Camera.transform, Vector3.up);
+    }
+}
---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Movement/FollowTarget.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offset;
+
+    private void Update()
+    {
+        transform.position = this.target.position + this.offset;
+    }
+}
