05ede073b6f94c824aafbb302e360be8f3e9b856
Lcnvdl <programando.soft@gmail.com>
2023-07-16 20:40:54 -0300

    RotateNoGC

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Actions/Movement/RotateNoGC.cs
@@ -0,0 +1,33 @@
+using CodeInUnity.Core.Utils;
+using UnityEngine;
+
+public class RotateNoGC : ActionScript
+{
+    public Transform target;
+
+    public float xSpeed = 0;
+    public float ySpeed = 0;
+    public float zSpeed = 0;
+
+    [SerializeField]
+    [HideInInspector]
+    private bool isEnabled = false;
+
+    protected override void Run()
+    {
+        isEnabled = true;
+    }
+
+    private void Update()
+    {
+        if (isEnabled)
+        {
+            if (target == null)
+            {
+                target = transform;
+            }
+
+            target.rotation = Quaternion.Euler(target.rotation.eulerAngles + new Vector3(xSpeed, ySpeed, zSpeed) * Time.deltaTime);
+        }
+    }
+}
