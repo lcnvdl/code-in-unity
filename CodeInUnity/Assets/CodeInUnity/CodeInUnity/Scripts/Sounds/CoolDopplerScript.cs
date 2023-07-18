2492a2a1f59d88fed2e0c267575663d85eedde8f
Lcnvdl <programando.soft@gmail.com>
2023-07-18 08:47:12 -0300

    Update CoolDopplerScript.cs

--- CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Sounds/CoolDopplerScript.cs
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Sounds/CoolDopplerScript.cs
@@ -6,12 +6,19 @@ public class CoolDopplerScript : MonoBehaviour
 
     public AudioHighPassFilter source;
 
+    public float minDistance = 1f;
+
     public float maxDistance = 10f;
 
     public float maxDisort = 1000f;
 
     void Update()
     {
-        source.cutoffFrequency = maxDisort * Mathf.Min(maxDistance, Vector3.Distance(listener.transform.position, source.transform.position)) / maxDistance;
+        float distance = Vector3.Distance(listener.transform.position, source.transform.position);
+        
+        if (distance > minDistance)
+        {
+            source.cutoffFrequency = maxDisort * Mathf.Min(maxDistance, (distance - minDistance)) / maxDistance;
+        }
     }
 }
