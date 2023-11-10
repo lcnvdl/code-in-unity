93a913ce70c99c3c622ec1d3acf1caae985b982b
Lucho <programando.soft@gmail.com>
2023-11-10 12:39:13 -0300

    Update SeededRandom.cs

--- CodeInUnity/Assets/CodeInUnity/CodeInUnity/Core/Security/SeededRandom.cs
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Core/Security/SeededRandom.cs
@@ -1,49 +1,69 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using S = System;
 
 namespace CodeInUnity.Core.Security
 {
-    [S.Serializable]
-    public class SeededRandom
+  [S.Serializable]
+  public class SeededRandom
+  {
+    [SerializeField]
+    [HideInInspector]
+    private int currentSeed = 1;
+
+    public int Seed
     {
-        [SerializeField]
-        [HideInInspector]
-        private int currentSeed = 1;
-
-        public int Seed
-        {
-            get => this.currentSeed;
-            set
-            {
-                this.randomInstance = null;
-                this.currentSeed = value;
-            }
-        }
-
-        private S.Random randomInstance = null;
-
-        private S.Random RandomInstance => this.randomInstance ?? (this.randomInstance = new S.Random(this.currentSeed));
-
-        public float Range(float minInclusive, float maxInclusive)
-        {
-            double next = this.RandomInstance.NextDouble();
-            return minInclusive + (float)((maxInclusive - minInclusive) * next);
-        }
-
-        public float Next(float maxInclusive)
-        {
-            double next = this.RandomInstance.NextDouble();
-            return (float)(maxInclusive * next);
-        }
-
-        public int Range(int minInclusive, int maxExclusive)
-        {
-            return this.RandomInstance.Next(minInclusive, maxExclusive);
-        }
-
-        public int Next(int maxExclusive)
-        {
-            return this.RandomInstance.Next(maxExclusive);
-        }
+      get => this.currentSeed;
+      set
+      {
+        this.randomInstance = null;
+        this.currentSeed = value;
+      }
     }
+
+    private S.Random randomInstance = null;
+
+    private S.Random RandomInstance => this.randomInstance ?? (this.randomInstance = new S.Random(this.currentSeed));
+
+    public float Range(float minInclusive, float maxInclusive)
+    {
+      double next = this.RandomInstance.NextDouble();
+      return minInclusive + (float)((maxInclusive - minInclusive) * next);
+    }
+
+    public double Next()
+    {
+      double next = this.RandomInstance.NextDouble();
+      return next;
+    }
+
+    public float Next(float maxInclusive)
+    {
+      double next = this.RandomInstance.NextDouble();
+      return (float)(maxInclusive * next);
+    }
+
+    public int Range(int minInclusive, int maxExclusive)
+    {
+      return this.RandomInstance.Next(minInclusive, maxExclusive);
+    }
+
+    public int Next(int maxExclusive)
+    {
+      return this.RandomInstance.Next(maxExclusive);
+    }
+
+    public void RandomizeTimeBased()
+    {
+      this.Seed = (int)(DateTime.Now.Ticks % 9999);
+    }
+
+    public static SeededRandom NewRandomized()
+    {
+      var random = new SeededRandom();
+      random.RandomizeTimeBased();
+
+      return random;
+    }
+  }
 }
