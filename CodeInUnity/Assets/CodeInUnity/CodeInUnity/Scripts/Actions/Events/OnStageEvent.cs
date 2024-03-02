d50dd48e5428f8e64b0d95875fa8058faa20bf77
Lucho <programando.soft@gmail.com>
2024-03-02 01:09:28 -0300

    Create OnStageEvent.cs

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Scripts/Actions/Events/OnStageEvent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace CodeInUnity.Scripts.Actions.Events
+{
+  public class OnStageEvent : MonoBehaviour
+  {
+    public UnityEvent onProduction;
+    
+    public UnityEvent onEditor;
+    
+    public UnityEvent onTest;
+
+    private void Awake()
+    {
+#if UNITY_EDITOR
+      bool isTest = Environment.StackTrace.Contains("UnityEngine.TestRunner");
+
+      if (isTest)
+      {
+        this.onTest.Invoke();
+      }
+      else
+      {
+        this.onEditor.Invoke();
+      }
+#else
+      this.onProduction.Invoke();
+#endif
+    }
+  }
+}
