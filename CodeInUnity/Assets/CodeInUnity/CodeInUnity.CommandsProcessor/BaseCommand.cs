2e80975f8bd7340974211da53bd1cf88b4d171db
Lucho <programando.soft@gmail.com>
2024-03-02 01:09:24 -0300

    Command dependencies

--- CodeInUnity/Assets/CodeInUnity/CodeInUnity.CommandsProcessor/BaseCommand.cs
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity.CommandsProcessor/BaseCommand.cs
@@ -1,148 +1,176 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace CodeInUnity.Command
 {
-    public enum CommandStatus
+  public enum CommandStatus
+  {
+    NotStarted,
+    Running,
+    Paused,
+    Finished,
+    Cancelled
+  }
+
+  [Serializable]
+  public abstract class BaseCommand
+  {
+    [HideInInspector]
+    public string uuid;
+
+    public string internalId;
+
+    [SerializeField]
+    private List<string> dependencies;
+
+    public bool isAsync = false;
+
+    [HideInInspector]
+    public int internalOrder = 0;
+
+    /// <summary>
+    /// Less means more priority.
+    /// </summary>
+    public int priority = 0;
+
+    public float timeout = 0;
+
+    [SerializeField]
+    [HideInInspector]
+    private CommandStatus status = CommandStatus.NotStarted;
+
+    public bool IsRunning => this.status == CommandStatus.Running;
+
+    /// <summary>
+    /// Pausado porque se asigno CPU a otro con más prioridad.
+    /// </summary>
+    public bool IsPaused => this.status == CommandStatus.Paused;
+
+    public bool IsCancelled => this.status == CommandStatus.Cancelled;
+
+    public bool IsFinished => this.status == CommandStatus.Finished;
+
+    public bool IsInFinishedStatus => this.IsFinished || this.IsCancelled;
+
+    public bool HasDependencies => this.dependencies != null && this.dependencies.Count > 0;
+
+    public List<string> Dependencies => this.dependencies;
+
+    public CommandStatus Status => this.status;
+
+    [SerializeField]
+    [HideInInspector]
+    private float totalTime = 0f;
+
+    public virtual float Progress => this.IsInFinishedStatus ? 1f : 0f;
+
+    public float TotalTime => this.totalTime;
+
+    public GameObject manualTarget = null;
+
+    [SerializeField]
+    [HideInInspector]
+    private GameObject initialGameObject = null;
+
+    public GameObject InitialGameObject => this.initialGameObject;
+
+    [SerializeField]
+    [HideInInspector]
+    private GameObject lastGameObject;
+
+    public GameObject LastGameObject
     {
-        NotStarted,
-        Running,
-        Paused,
-        Finished,
-        Cancelled
+      get => this.lastGameObject;
+      private set => this.lastGameObject = value;
     }
 
-    [Serializable]
-    public abstract class BaseCommand
+    public BaseCommand()
     {
-        [HideInInspector]
-        public string uuid;
-
-        public string internalId;
-
-        public bool isAsync = false;
-
-        [HideInInspector]
-        public int internalOrder = 0;
-
-        /// <summary>
-        /// Less means more priority.
-        /// </summary>
-        public int priority = 0;
-
-        public float timeout = 0;
-
-        [SerializeField]
-        [HideInInspector]
-        private CommandStatus status = CommandStatus.NotStarted;
-
-        public bool IsRunning => this.status == CommandStatus.Running;
-
-        /// <summary>
-        /// Pausado porque se asigno CPU a otro con más prioridad.
-        /// </summary>
-        public bool IsPaused => this.status == CommandStatus.Paused;
-
-        public bool IsCancelled => this.status == CommandStatus.Cancelled;
-
-        public bool IsFinished => this.status == CommandStatus.Finished;
-
-        public bool IsInFinishedStatus => this.IsFinished || this.IsCancelled;
-
-        public CommandStatus Status => this.status;
-
-        [SerializeField]
-        [HideInInspector]
-        private float totalTime = 0f;
-
-        public virtual float Progress => this.IsInFinishedStatus ? 1f : 0f;
-
-        public float TotalTime => this.totalTime;
-
-        public GameObject manualTarget = null;
-
-        [SerializeField]
-        [HideInInspector]
-        private GameObject initialGameObject = null;
-
-        public GameObject InitialGameObject => this.initialGameObject;
-
-        [SerializeField]
-        [HideInInspector]
-        private GameObject lastGameObject;
-
-        public GameObject LastGameObject
-        {
-            get => this.lastGameObject;
-            private set => this.lastGameObject = value;
-        }
-
-        public BaseCommand()
-        {
-            this.uuid = Guid.NewGuid().ToString();
-        }
-
-        public BaseCommand(string uuid)
-        {
-            this.uuid = uuid;
-        }
-
-        public virtual void Start(GameObject gameObject)
-        {
-            this.status = CommandStatus.Running;
-            this.initialGameObject = gameObject;
-            this.LastGameObject = gameObject;
-            //Debug.Log(this.GetType().Name + " started");
-        }
-
-        public virtual void Pause()
-        {
-            this.status = CommandStatus.Paused;
-            //Debug.Log(this.GetType().Name + " paused");
-        }
-
-        public virtual void Unpause()
-        {
-            this.status = CommandStatus.Running;
-            //Debug.Log(this.GetType().Name + " unpaused");
-        }
-
-        public void Step(float dt, GameObject gameObject)
-        {
-            GameObject target = this.manualTarget == null ? gameObject : this.manualTarget;
-            this.LastGameObject = target;
-            this.Work(dt, target);
-            totalTime += dt;
-
-            if (this.timeout > 0 && this.IsRunning && this.totalTime > this.timeout)
-            {
-                this.Cancel("Timeout");
-            }
-        }
-
-        public virtual void Cancel(string reason = null)
-        {
-            this.status = CommandStatus.Cancelled;
-            //Debug.Log(this.GetType().Name + " cancelled " + (reason ?? string.Empty));
-            this.OnCancelOrFinish();
-        }
-
-        protected virtual void Finish()
-        {
-            this.status = CommandStatus.Finished;
-            //Debug.Log(this.GetType().Name + " finished");
-            this.OnCancelOrFinish();
-        }
-
-        protected virtual void OnCancelOrFinish()
-        {
-        }
-
-        protected abstract void Work(float dt, GameObject gameObject);
-
-        public BaseCommand Clone()
-        {
-            return this.MemberwiseClone() as BaseCommand;
-        }
+      this.uuid = Guid.NewGuid().ToString();
     }
+
+    public BaseCommand(string uuid)
+    {
+      this.uuid = uuid;
+    }
+
+    public virtual void Start(GameObject gameObject)
+    {
+      this.status = CommandStatus.Running;
+      this.initialGameObject = gameObject;
+      this.LastGameObject = gameObject;
+      //Debug.Log(this.GetType().Name + " started");
+    }
+
+    public virtual void Pause()
+    {
+      this.status = CommandStatus.Paused;
+      //Debug.Log(this.GetType().Name + " paused");
+    }
+
+    public virtual void Unpause()
+    {
+      this.status = CommandStatus.Running;
+      //Debug.Log(this.GetType().Name + " unpaused");
+    }
+
+    public void Step(float dt, GameObject gameObject)
+    {
+      GameObject target = this.manualTarget == null ? gameObject : this.manualTarget;
+      this.LastGameObject = target;
+      this.Work(dt, target);
+      totalTime += dt;
+
+      if (this.timeout > 0 && this.IsRunning && this.totalTime > this.timeout)
+      {
+        this.Cancel("Timeout");
+      }
+    }
+
+    public virtual void Cancel(string reason = null)
+    {
+      this.status = CommandStatus.Cancelled;
+      //Debug.Log(this.GetType().Name + " cancelled " + (reason ?? string.Empty));
+      this.OnCancelOrFinish();
+    }
+
+    public void AddDependency(string name)
+    {
+      if (this.dependencies == null)
+      {
+        this.dependencies = new List<string>();
+      }
+
+      this.dependencies.Add(name);
+    }
+
+    public void RemoveDependency(string name)
+    {
+      if (this.dependencies == null)
+      {
+        return;
+      }
+
+      this.dependencies.Remove(name);
+    }
+
+    protected virtual void Finish()
+    {
+      this.status = CommandStatus.Finished;
+      //Debug.Log(this.GetType().Name + " finished");
+      this.OnCancelOrFinish();
+    }
+
+    protected virtual void OnCancelOrFinish()
+    {
+    }
+
+    protected abstract void Work(float dt, GameObject gameObject);
+
+    public BaseCommand Clone()
+    {
+      return this.MemberwiseClone() as BaseCommand;
+    }
+  }
 }
--- CodeInUnity/Assets/CodeInUnity/CodeInUnity.CommandsProcessor/CommandsListener.cs
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity.CommandsProcessor/CommandsListener.cs
@@ -5,262 +5,308 @@ using CodeInUnity.Command;
 
 public class CommandsListener : MonoBehaviour
 {
-    public bool manualRun = false;
+  public bool manualRun = false;
 
-    public bool runOnFixedUpdate = false;
+  public bool runOnFixedUpdate = false;
 
-    public bool isPaused = false;
+  public bool isPaused = false;
 
-    [SerializeReference]
-    public List<BaseCommand> commands = new List<BaseCommand>();
+  [SerializeReference]
+  public List<BaseCommand> commands = new List<BaseCommand>();
 
-    public bool IsWorking
+  private List<BaseCommand> withDependencies = new List<BaseCommand>();
+
+  private List<string> deletedCommands = new List<string>();
+
+  public bool IsWorking
+  {
+    get
     {
-        get
+      for (int i = 0; i < this.commands.Count; i++)
+      {
+        if (this.commands[i].IsRunning)
         {
-            for (int i = 0; i < this.commands.Count; i++)
-            {
-                if (this.commands[i].IsRunning)
-                { 
-                    return true;
-                }
-            }
-
-            return false;
+          return true;
         }
+      }
+
+      return false;
     }
+  }
 
-    public bool HasCommands => this.commands.Count > 0;
+  public bool HasCommands => this.commands.Count > 0;
 
-    public bool IsEmpty
+  public bool IsEmpty
+  {
+    get
     {
-        get
-        {
-            if (this.commands.Count == 0)
-            {
-                return true;
-            }
-
-            for (int i = 0; i < this.commands.Count; i++)
-            {
-                if (!this.commands[i].IsInFinishedStatus)
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (!this.manualRun && this.runOnFixedUpdate)
-        {
-            this.NextStep(Time.fixedDeltaTime);
-        }
-    }
-
-    private void Update()
-    {
-        if (!this.manualRun && !this.runOnFixedUpdate)
-        {
-            this.NextStep(Time.deltaTime);
-        }
-    }
-
-    public virtual void NextStep(float dt)
-    {
-        if (this.isPaused)
-        {
-            return;
-        }
-
-        SortAndClearCommands();
-        HandleAsyncCommands(dt);
-        HandleSyncCommands(dt);
-    }
-
-    public void AddCommand(BaseCommand cmd)
-    {
-        cmd.internalOrder = this.NextInternalOrder();
-        this.commands.Add(cmd);
-    }
-
-    public bool AddUniqueCommand(BaseCommand cmd)
-    {
-        if (this.HasCommand(cmd.internalId))
-        {
-            return false;
-        }
-
-        this.AddCommand(cmd);
-
+      if (this.commands.Count == 0)
+      {
         return true;
-    }
+      }
 
-    public T GetCommand<T>() where T : BaseCommand
-    {
-        return (T)this.commands.Find(m => m is T);
-    }
-
-    public BaseCommand GetCommand(string id)
-    {
-        return this.commands.Find(m => m.internalId == id);
-    }
-
-    public void CancelCommand(string id, string reason = null)
-    {
-        this.commands.Find(m => m.internalId == id)?.Cancel(reason);
-    }
-
-    public T GetCommand<T>(string id) where T : BaseCommand
-    {
-        return (T)this.commands.Find(m => m.internalId == id);
-    }
-
-    public bool HasCommand(string id)
-    {
-        int count = this.commands.Count;
-        
-        for (int i = 0; i < count; i++)
+      for (int i = 0; i < this.commands.Count; i++)
+      {
+        if (!this.commands[i].IsInFinishedStatus)
         {
-            if (this.commands[i].internalId == id)
-            {
-                return true;
-            }
+          return false;
+        }
+      }
+
+      return true;
+    }
+  }
+
+  private void FixedUpdate()
+  {
+    if (!this.manualRun && this.runOnFixedUpdate)
+    {
+      this.NextStep(Time.fixedDeltaTime);
+    }
+  }
+
+  private void Update()
+  {
+    if (!this.manualRun && !this.runOnFixedUpdate)
+    {
+      this.NextStep(Time.deltaTime);
+    }
+  }
+
+  public virtual void NextStep(float dt)
+  {
+    if (this.isPaused)
+    {
+      return;
+    }
+
+    SortAndClearCommands();
+    HandleAsyncCommands(dt);
+    HandleSyncCommands(dt);
+  }
+
+  public void AddCommand(BaseCommand cmd)
+  {
+    cmd.internalOrder = this.NextInternalOrder();
+    this.commands.Add(cmd);
+  }
+
+  public bool AddUniqueCommand(BaseCommand cmd)
+  {
+    if (this.HasCommand(cmd.internalId))
+    {
+      return false;
+    }
+
+    this.AddCommand(cmd);
+
+    return true;
+  }
+
+  public T GetCommand<T>() where T : BaseCommand
+  {
+    return (T)this.commands.Find(m => m is T);
+  }
+
+  public BaseCommand GetCommand(string id)
+  {
+    return this.commands.Find(m => m.internalId == id);
+  }
+
+  public void CancelCommand(string id, string reason = null)
+  {
+    this.commands.Find(m => m.internalId == id)?.Cancel(reason);
+  }
+
+  public T GetCommand<T>(string id) where T : BaseCommand
+  {
+    return (T)this.commands.Find(m => m.internalId == id);
+  }
+
+  public bool HasCommand(string id)
+  {
+    int count = this.commands.Count;
+
+    for (int i = 0; i < count; i++)
+    {
+      if (this.commands[i].internalId == id)
+      {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public void CancelCommands(string reason)
+  {
+    commands.FindAll(m => !m.IsFinished).ForEach(m => m.Cancel(reason));
+    commands.Clear();
+  }
+
+  private void SortAndClearCommands()
+  {
+    int len = this.commands.Count;
+
+    if (len == 0)
+    {
+      return;
+    }
+
+    this.withDependencies.Clear();
+    this.deletedCommands.Clear();
+
+    for (int i = len - 1; i >= 0; i--)
+    {
+      var command = this.commands[i];
+
+      if (command.IsInFinishedStatus)
+      {
+        if (this.withDependencies.Count > 0)
+        {
+          this.deletedCommands.Add(command.uuid);
         }
 
-        return false;
+        this.commands.RemoveAt(i);
+      }
+      else if (command.HasDependencies)
+      {
+        this.withDependencies.Add(command);
+      }
     }
 
-    public void CancelCommands(string reason)
+    this.UpdateDependenciesAfterDeleteCommands();
+
+    if (this.commands.Count > 1)
     {
-        commands.FindAll(m => m.IsFinished).ForEach(m => m.Cancel(reason));
-        commands.Clear();
+      this.commands.SortNA((a, b) => SortCommands(a, b));
     }
+  }
 
-    private void SortAndClearCommands()
+  private void HandleSyncCommands(float dt)
+  {
+    var nextSyncCommand = this.PickAndEnableNextCommand();
+
+    if (nextSyncCommand != null)
     {
-        int len = this.commands.Count;
+      nextSyncCommand.Step(dt, gameObject);
 
-        if (len == 0)
-        {
-            return;
-        }
-
-        //  GC Optimization (0 bytes)
-        this.commands.RemoveAll(m => m.IsInFinishedStatus);
-
-        if (this.commands.Count > 1)
-        {
-            this.commands.SortNA((a, b) => SortCommands(a, b));
-        }
+      if (!nextSyncCommand.IsRunning)
+      {
+        PickAndEnableNextCommand();
+      }
     }
+  }
 
-    private void HandleSyncCommands(float dt)
+  private void UpdateDependenciesAfterDeleteCommands()
+  {
+    if (this.deletedCommands.Count > 0)
     {
-        var nextSyncCommand = this.PickAndEnableNextCommand();
-
-        if (nextSyncCommand != null)
+      foreach (var cmd in this.withDependencies)
+      {
+        foreach (var deletedId in this.deletedCommands)
         {
-            nextSyncCommand.Step(dt, gameObject);
-
-            if (!nextSyncCommand.IsRunning)
-            {
-                PickAndEnableNextCommand();
-            }
+          cmd.RemoveDependency(deletedId);
         }
+      }
     }
-
-    private BaseCommand PickAndEnableNextCommand()
+    else if (this.withDependencies.Count > 0)
     {
-        if (commands.Count == 0)
-        {
-            return null;
-        }
-
-        var nextSyncCommand = commands.Find(m => !m.isAsync && !m.IsInFinishedStatus);
-
-        if (nextSyncCommand != null)
-        {
-            if (nextSyncCommand.Status == CommandStatus.NotStarted)
-            {
-                this.PauseAllRunningSyncCommands();
-
-                nextSyncCommand.Start(gameObject);
-            }
-            else if (nextSyncCommand.IsPaused)
-            {
-                nextSyncCommand.Unpause();
-            }
-        }
-
-        return nextSyncCommand;
+      foreach (var cmd in this.withDependencies)
+      {
+        cmd.Dependencies.RemoveAll(m => !this.HasCommand(m));
+      }
     }
+  }
 
-    private void PauseAllRunningSyncCommands()
+  private BaseCommand PickAndEnableNextCommand()
+  {
+    if (this.commands.Count == 0)
     {
-        foreach (var cmd in commands)
-        {
-            if (cmd.IsRunning && !cmd.isAsync)
-            {
-                cmd.Pause();
-            }
-        }
+      return null;
     }
 
-    private int NextInternalOrder()
+    var nextSyncCommand = this.commands.Find(m => !m.isAsync && !m.IsInFinishedStatus && !m.HasDependencies);
+
+    if (nextSyncCommand != null)
     {
-        if (commands.Count == 0)
-        {
-            return 0;
-        }
+      if (nextSyncCommand.Status == CommandStatus.NotStarted)
+      {
+        this.PauseAllRunningSyncCommands();
 
-        int max = 0;
-
-        foreach (var command in commands)
-        {
-            if (command.internalOrder > max)
-            {
-                max = command.internalOrder;
-            }
-        }
-
-        return max + 1;
+        nextSyncCommand.Start(gameObject);
+      }
+      else if (nextSyncCommand.IsPaused)
+      {
+        nextSyncCommand.Unpause();
+      }
     }
 
-    private void HandleAsyncCommands(float dt)
+    return nextSyncCommand;
+  }
+
+  private void PauseAllRunningSyncCommands()
+  {
+    foreach (var cmd in commands)
     {
-        if (commands.Count == 0)
-        {
-            return;
-        }
-
-        foreach (var cmd in commands)
-        {
-            if (cmd.isAsync && !cmd.IsPaused)
-            {
-                if (cmd.Status == CommandStatus.NotStarted)
-                {
-                    cmd.Start(gameObject);
-                }
-
-                if (cmd.Status == CommandStatus.Running)
-                {
-                    cmd.Step(dt, gameObject);
-                }
-            }
-        }
+      if (cmd.IsRunning && !cmd.isAsync)
+      {
+        cmd.Pause();
+      }
     }
+  }
 
-    private static int SortCommands(BaseCommand x, BaseCommand y)
+  private int NextInternalOrder()
+  {
+    if (this.commands.Count == 0)
     {
-        if (x.priority != y.priority)
+      return 0;
+    }
+
+    int max = 0;
+
+    foreach (var command in this.commands)
+    {
+      if (command.internalOrder > max)
+      {
+        max = command.internalOrder;
+      }
+    }
+
+    return max + 1;
+  }
+
+  private void HandleAsyncCommands(float dt)
+  {
+    if (this.commands.Count == 0)
+    {
+      return;
+    }
+
+    foreach (var cmd in commands)
+    {
+      if (cmd.isAsync && !cmd.IsPaused && !cmd.HasDependencies)
+      {
+        if (cmd.Status == CommandStatus.NotStarted)
         {
-            return y.priority - x.priority;
+          cmd.Start(gameObject);
         }
 
-        return x.internalOrder - y.internalOrder;
+        if (cmd.Status == CommandStatus.Running)
+        {
+          cmd.Step(dt, gameObject);
+        }
+      }
     }
+  }
+
+  private static int SortCommands(BaseCommand x, BaseCommand y)
+  {
+    if (x.priority != y.priority)
+    {
+      return y.priority - x.priority;
+    }
+
+    return x.internalOrder - y.internalOrder;
+  }
 }
