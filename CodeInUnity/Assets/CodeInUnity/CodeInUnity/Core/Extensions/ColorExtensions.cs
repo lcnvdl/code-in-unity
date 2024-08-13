53fa5d2af0f2f1e04593fc0379f20ba23c238a10
Lucho <programando.soft@gmail.com>
2024-08-13 02:13:56 -0300

    ColorExtensions

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Core/Extensions/ColorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace CodeInUnity.Extensions
+{
+  public static class ColorExtensions
+  {
+    public static Color SetAlpha(this Color c, float a)
+    {
+      return new Color(c.r, c.g, c.b, a);
+    }
+  }
+}
