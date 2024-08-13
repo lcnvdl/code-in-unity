350da3884057648f1ccd659060f0bec72edf2679
Lucho <programando.soft@gmail.com>
2024-08-13 02:14:04 -0300

    EnaboeObjectCommand

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity.CommandsProcessor/Commands/EnableObjectCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using CodeInUnity.CommandsProcessor;
+
+namespace CodeInUnity.Commands
+{
+  [Serializable]
+  public class EnableObjectCommand : BaseCommand
+  {
+    public GameObject targetObject;
+
+    public float delay = 0f;
+
+    public bool enableTargetObject = true;
+
+    protected override void Work(float dt, GameObject gameObject)
+    {
+      if (this.delay > 0f)
+      {
+        this.delay -= dt;
+        return;
+      }
+
+      if (this.targetObject != null)
+      {
+        this.targetObject.SetActive(this.enableTargetObject);
+      }
+
+      this.Finish();
+    }
+  }
+}
