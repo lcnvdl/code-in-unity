172a1d44113480781a0d4261cc432114bc6c4439
Lucho <programando.soft@gmail.com>
2024-10-10 01:30:28 -0300

    MaterialExtensions

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Core/Extensions/MaterialExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace CodeInUnity.Extensions
+{
+  public static class MaterialExtensions
+  {
+    public static Material Clone(this Material material)
+    {
+      return new Material(material);
+    }
+  }
+}
