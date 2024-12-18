54e4e5542f4203b9128223d26b1638690f535a79
Lucho <programando.soft@gmail.com>
2024-12-18 12:07:01 -0300

    Environment utils.

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Core/Utils/EnvironmentUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeInUnity.Core.Utils
+{
+  public static class EnvironmentUtils
+  {
+#if UNITY_EDITOR
+    public static bool IsInTestMode => Application.isEditor && Environment.StackTrace.Contains("UnityEngine.TestRunner");
+#else
+    public const bool IsInTestMode = false;
+#endif
+  }
+}
