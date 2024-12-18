7b780f5b152189439dc1eb333d82f7b04b5d8b8f
Lucho <programando.soft@gmail.com>
2024-12-18 12:22:09 -0300

    Update StringUtilsTest.cs

--- CodeInUnity/Assets/Tests/Editor/Core/Utils/StringUtilsTest.cs
+++ CodeInUnity/Assets/Tests/Editor/Core/Utils/StringUtilsTest.cs
@@ -5,20 +5,41 @@ using UnityEngine;
 
 namespace Tests
 {
-    public class StringUtilsTest
+  public class StringUtilsTest
+  {
+    [Test]
+    public void FastEndsWith_True()
     {
-        [Test]
-        public void FastEndsWith_True()
-        {
-            bool result = StringUtils.FastEndsWith("This is a text", "text");
-            Assert.IsTrue(result);
-        }
-
-        [Test]
-        public void FastEndsWith_False()
-        {
-            bool result = StringUtils.FastEndsWith("This is a text", "test");
-            Assert.IsFalse(result);
-        }
+      bool result = StringUtils.FastEndsWith("This is a text", "text");
+      Assert.IsTrue(result);
     }
+
+    [Test]
+    public void FastEndsWith_False()
+    {
+      bool result = StringUtils.FastEndsWith("This is a text", "test");
+      Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void CountOccurences_ShouldWorkFine()
+    {
+      int result = StringUtils.CountOccurences("this is a text", 't');
+      Assert.AreEqual(3, result);
+    }
+
+    [Test]
+    public void CountOccurences_MustNotIgnoreCase()
+    {
+      int result = StringUtils.CountOccurences("This is a text", 't');
+      Assert.AreEqual(2, result);
+    }
+
+    [Test]
+    public void CountOccurences_ShouldReturnZero_IfStringIsEmpty()
+    {
+      int result = StringUtils.CountOccurences("", 't');
+      Assert.AreEqual(0, result);
+    }
+  }
 }
