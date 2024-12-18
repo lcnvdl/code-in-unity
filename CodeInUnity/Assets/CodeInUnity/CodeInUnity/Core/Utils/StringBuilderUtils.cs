7d54e0f8ab2d69bf56b2ca567189c87a074c5f10
Lucho <programando.soft@gmail.com>
2024-12-18 12:16:04 -0300

    StringBuilderUtils

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Core/Utils/StringBuilderUtils.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace CodeInUnity.Core.Utils
+{
+  public class StringBuilderUtils
+  {
+    public static int CountOccurences(StringBuilder sb, char c)
+    {
+      int len = sb.Length;
+      int count = 0;
+
+      for (int i = 0; i < len; i++)
+      {
+        if (sb[i].Equals(c))
+        {
+          count++;
+        }
+      }
+
+      return count;
+    }
+  }
+}
