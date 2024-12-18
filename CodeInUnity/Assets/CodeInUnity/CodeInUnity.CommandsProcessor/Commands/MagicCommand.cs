8ebca421f7185ab8a5bd2280bf6b381ad924faf2
Lucho <programando.soft@gmail.com>
2024-12-18 12:16:58 -0300

    Update MagicCommand.cs

--- CodeInUnity/Assets/CodeInUnity/CodeInUnity.CommandsProcessor/Commands/MagicCommand.cs
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity.CommandsProcessor/Commands/MagicCommand.cs
@@ -4,22 +4,31 @@ using CodeInUnity.CommandsProcessor;
 
 namespace CodeInUnity.Commands
 {
-    public class MagicCommand : BaseCommand
+  public class MagicCommand : BaseCommand
+  {
+    public float currentProgress = 0;
+
+    public Action<MagicCommand, float, GameObject> action;
+
+    public override float Progress => currentProgress;
+
+    public MagicCommand()
     {
-        public float currentProgress = 0;
-
-        public Action<MagicCommand, float, GameObject> action;
-
-        public override float Progress => currentProgress;
-
-        protected override void Work(float dt, GameObject gameObject)
-        {
-            this.action?.Invoke(this, dt, gameObject);
-        }
-
-        public void FinishCommand()
-        {
-            this.Finish();
-        }
     }
+
+    public MagicCommand(Action<MagicCommand, float, GameObject> action)
+    {
+      this.action = action;
+    }
+
+    protected override void Work(float dt, GameObject gameObject)
+    {
+      this.action?.Invoke(this, dt, gameObject);
+    }
+
+    public void FinishCommand()
+    {
+      this.Finish();
+    }
+  }
 }
