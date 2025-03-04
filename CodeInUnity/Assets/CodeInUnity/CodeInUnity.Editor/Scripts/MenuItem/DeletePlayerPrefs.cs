e7b3d1099bb7868852e5ddec26a55d1d5744dd5a
Lucho <programando.soft@gmail.com>
2025-03-04 04:10:42 -0300

    DepetePlayerPrefs menu item added.

---
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity.Editor/Scripts/MenuItem/DeletePlayerPrefs.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CodeInUnity.Editor.Scripts
+{
+  public class DeletePlayerPrefs : MonoBehaviour
+  {
+    [MenuItem("Tools/Code in Unity/Delete PlayerPrefs")]
+    public static void DoDeletePlayerPrefs()
+    {
+      PlayerPrefs.DeleteAll();
+      Debug.Log("PlayerPrefs deleted");
+    }
+  }
+}
