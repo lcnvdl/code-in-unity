cfc444413844ca5859d06406d4f76af4c38c7c88
Lucho <programando.soft@gmail.com>
2025-03-19 10:45:07 -0300

    Update ISmoothTask.cs

--- CodeInUnity/Assets/CodeInUnity/CodeInUnity/Core/Interfaces/ISmoothTask.cs
+++ CodeInUnity/Assets/CodeInUnity/CodeInUnity/Core/Interfaces/ISmoothTask.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeInUnity.Interfaces
+﻿namespace CodeInUnity.Interfaces
 {
   public interface ISmoothTask
   {
